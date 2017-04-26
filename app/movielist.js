d718866887a414d86a143067c709cbc3218e88cc
Saurabh Singhal <singhalsaurabh95@gmail.com>
2017-04-26 15:21:53 +0530

    Added API call

--- app/movielist.js
+++ app/movielist.js
@@ -4,16 +4,31 @@ import {
   Text,
   View
 } from 'react-native';
+import TMDB_KEY from './key';
+
+
 
 class MoviesComponent extends Component {
   static navigationOptions = {
     title: 'Movies',
   };
 
+loadMovies() {
+  console.log(TMDB_KEY);
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key="+TMDB_KEY)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+    })
+    .catch((error) => console.error(error));
+  }
+
   constructor(props) {
     super(props);
+    this.loadMovies();
 
   }
+
     render(){
       return (
         <View style={{flex: 1, backgroundColor: 'green'}}>
